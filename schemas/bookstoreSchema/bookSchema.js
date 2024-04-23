1c2ddfb473496081e58cfefe51a19f063636fa47
umair jamaat ali <umairansari758@gmail.com>
2024-04-23 17:52:15 +0500

    book schema

---
+++ schemas/bookstoreSchema/bookSchema.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import { model } from "mongoose";
+const BookInfo= new mongoose.Schema({
+    // bookinfo: {
+    //     title: { type: String, required: [true,"Title is required"] },
+    //     author: { type: String, required: [true,"Author name is required"] },
+    //     description: { type: String, required:[true, "Please write description of book"] },
+    //     // Add other relevant book information fields here
+    //   },
+    title:{
+        type:String,
+        required:[true,"Title is required"],
+        maxLength: [200, "Too much length"],
+        trim:true
+    },
+    userid:{
+        type:mongoose.Schema.ObjectId,
+        ref:'users',
+        required:[true,"user is required"]
+    },
+    author:{
+        type:String,
+        maxLength: [200, "Too much length"],
+        trim:true,
+        required:[true,"Author name is required"]
+    },
+    price:{
+        type:Number,
+        required:[true,"Price is required"],
+        max: [200000000000000000, "Too much price"],
+        trim:true
+    },
+    category:{
+        type:String,
+        required:[true,"Please select category"],
+        maxLength: [200, "Too much length"],
+        trim:true
+    },
+    description:{
+        type:String,
+        required:[true, "Please write description of book"],
+        maxLength: [200000, "Too much length"],
+        trim:true
+    },
+    imgs_url:{
+            type:Array,
+            default:[],
+            required:[true, "Please add images"]
+        },
+     
+},{ timestamps: true })
+
+export default mongoose.models.books || mongoose.model("books",BookInfo);
